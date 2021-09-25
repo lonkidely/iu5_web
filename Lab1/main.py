f1c9dd7204b208ec1fb75de27453d9a7ae3969e9
lonkidely <lonkidely@gmail.com>
2021-09-25 23:52:01 +0300

    Added lab1 solution

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,16 +1,79 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import math
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def is_coef_correct(index, coef):
+    try:
+        num = float(coef)
+    except ValueError:
+        return False
+    if num == 0.0 and index == 1:
+        return False
+    return True
 
 
-# Press the green button in the gutter to run the script.
+def get_coef(index, message):
+    try:
+        coef = sys.argv[index]
+    except IndexError:
+        print(message)
+        coef = input()
+        while not (is_coef_correct(index, coef)):
+            print("Введенный коэффициент некорректен, введите другой")
+            coef = input()
+    return float(coef)
+
+
+def get_roots(a, b, c):
+    roots = []
+
+    d = b * b - 4.0 * a * c
+    if d < 0.0:
+        return
+    if d == 0.0:
+        root = -b / (2.0 * a)
+        if root == 0.0:
+            roots.append(0)
+        elif root > 0.0:
+            roots.append(math.sqrt(root))
+            roots.append(-math.sqrt(root))
+    else:
+        sqrt_d = math.sqrt(d)
+        first_root = (-b + sqrt_d) / (2.0 * a)
+        second_root = (-b - sqrt_d) / (2.0 * a)
+        if first_root == 0 or second_root == 0:
+            roots.append(0)
+        if first_root > 0.0:
+            roots.append(math.sqrt(first_root))
+            roots.append(-math.sqrt(first_root))
+        if second_root > 0.0:
+            roots.append(math.sqrt(second_root))
+            roots.append(-math.sqrt(second_root))
+
+    return roots
+
+
+def main():
+    a = get_coef(1, "Введите значение коэффициента A")
+    b = get_coef(2, "Введите значение коэффициента B")
+    c = get_coef(3, "Введите значение коэффициента C")
+
+    roots = get_roots(a, b, c)
+    cnt_roots = 0
+
+    try:
+        cnt_roots = len(roots)
+    except TypeError:
+        print("Корней нет!")
+        return
+
+    if cnt_roots == 0:
+        print("Корней нет!")
+    else:
+        print("Корни данного уравнения:")
+        for root in roots:
+            print(root, end = " ")
+        print()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
