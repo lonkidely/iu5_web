7d432a8e60139ba66a8d9bb9bcd1aef033e387c8
lonkidely <inkandies.social@gmail.com>
2021-10-12 15:35:39 +0300

    Changed Python version

--- Lab3/main.py
+++ Lab3/main.py
@@ -3,15 +3,16 @@ from lab_python_fp.field import field
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
     {'title': 'Диван для отдыха', 'color': 'black'},
-    {'title': None, 'color': None, 'price': 213},
+    {'title': None, 'color': 'white', 'price': None},
     {'title': None, 'price': 1234}
 ]
+
 
 def task1():
     for i in field(goods, 'title'):
         print(i)
 
-    for i in field(goods, 'title', 'color'):
+    for i in field(goods, 'title', 'price'):
         print(i)
 
 
